5b50811cb2e96c37da5a2bee6c80add2ca761d75
yutaro <y.matsuo1201@gmail.com>
2018-01-25 00:27:25 +0000

    データを取得

--- indeed/spiders/sp_indeed.py
+++ indeed/spiders/sp_indeed.py
@@ -5,7 +5,16 @@ import scrapy
 class SpIndeedSpider(scrapy.Spider):
     name = 'sp_indeed'
     allowed_domains = ['indeed.com']
-    start_urls = ['http://indeed.com/']
+    start_urls = ['https://jp.indeed.com/%E6%B1%82%E4%BA%BA?q=%E5%96%B6%E6%A5%AD&l=%E6%9D%B1%E4%BA%AC%E9%83%BD']
 
     def parse(self, response):
-        pass
+        for caset in response.css('div.result'):
+            if(caset.css('.company a')):
+                comp_name = caset.css('.company a::text').re_first('\n\s*(.*)')
+            else:
+                comp_name = caset.css('.company::text').re_first('\n\s*(.*)')
+            yield {
+                'comp_name': comp_name,
+                'comp_link': caset.css('.company a::attr(href)').extract_first(),
+                'review': caset.css('a span.slNoUnderline::text').re_first('([0-9]*)'),
+            }
